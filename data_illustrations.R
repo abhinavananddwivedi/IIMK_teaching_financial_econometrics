7c6cd9bf070e79fc1d7f933c19280af1c8c42294
unknown <abhinavananddwivedi@gmail.comgit config --global user.email abhinavananddwivedi@gmail.comgit config --global user.email abhinavananddwivedi@gmail.comgit config --global user.name abhinavananddwivedi>
2025-11-04 17:09:25 +0530

    first attempts at illustrating

---
+++ data_illustrations.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+theme_set(theme_bw())
+
+data_file <- readr::read_csv('gapminder_merged_long.csv') %>%
+  dplyr::filter(year <= 2025 & year >= 1947)
+
+data_Ind <- data_file %>% 
+  dplyr::filter(name == 'India') 
+
+plot_Ind_gdppc <- ggplot(data = data_Ind, 
+                         mapping = aes(x = year, y = gdp_pcap_21)) +
+  geom_point() +
+  geom_line() 
+
+plot_Ind_lifeexp <- ggplot(data = data_Ind, 
+                           mapping = aes(x = year, y = life_expectancy)) +
+  geom_point() +
+  geom_line() 
+
+plot_Ind_gdppc_lifeexp <- ggplot(data = data_Ind, 
+                                 mapping = aes(x = gdp_pcap, 
+                                               y = life_expectancy)) +
+  geom_point() +
+  geom_line() +
+  geom_smooth(method = 'lm', linetype = 'dotdashed') +
+  labs(x = 'GDP/capita (2021 USD)',
+       y = 'Life Expectancy',
+       title = 'India after independence',
+       )
+
+country_set <- c('India', 'China', 'France', 'Germany', 'UK', 'USA',
+                 'Brazil', 'Russia')
+
+plot_lifeexp_country_set <- ggplot(data = data_file %>% 
+                                     dplyr::filter(name %in% country_set), 
+                                   mapping = aes(x = year, 
+                                                 y = life_expectancy
+                                                 )
+                                   ) +
+  geom_point(mapping = aes(alpha = name)) +
+  geom_line(mapping = aes(linetype = name)) 
+  
